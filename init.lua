112dd09dd4be3ecc057851e8e036f3f1e4df04d7
Ben Datko <8881506+bdatko@users.noreply.github.com>
2025-11-30 16:05:56 -0500

    add better globals

--- init.lua
+++ init.lua
@@ -1,3 +1,17 @@
+P = function(v)
+  print(vim.inspect(v))
+  return v
+end
+
+if pcall(require, "plenary") then
+  RELOAD = require("plenary.reload").reload_module
+
+  R = function(name)
+    RELOAD(name)
+    return require(name)
+  end
+end
+
 -- map leader to space
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
